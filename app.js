05349edb5a85d74330845d4d7902fd3057581389
saidavaas <saidavaas.dev@gmail.com>
2025-09-08 20:20:55 -0600

    Create all features

--- app.js
+++ app.js
@@ -3,18 +3,51 @@ let friends = []
 function agregarAmigo(){
     let name = document.getElementById('amigo').value; 
     if (name == ''){
-        alert("Por favor, inserte un nombre.")
+        alert("Por favor, inserte un nombre.");
     }
     else{
-        friends.push(name)
-        console.log(friends)
+        
+        friends.push(title(name)); 
+        console.log(friends);
     }
+    tranverseFriends(); 
     clearBox(); 
 }
 
 function clearBox(){
     document.getElementById("amigo").value = ''; 
 }
+
+function tranverseFriends(){
+    let list = document.getElementById("listaAmigos"); 
+    
+    list.innerHTML = ""; 
+
+    for(let i=0; i<friends.length; i++){
+        list.innerHTML += `<li align=center>${friends[i]}</li>`; 
+
+    }
+}
+
+function sortearAmigo(){
+    
+    if(friends.length>0){
+      let random = Math.floor(Math.random() * friends.length);
+      console.log(random);
+      let secretFriend = friends[random];
+      console.log(secretFriend);
+      let result = document.getElementById("resultado");
+      result.innerHTML = `<p>${secretFriend}</p`; 
+    
+    }
+}
+
+function title(str){
+    return str.charAt(0).toUpperCase()+str.slice(1); 
+
+}
+
+    
 
 
 
